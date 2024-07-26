107f4a40f132dcb7e3dce022850afd6aeef1105b
Hellen Moura <hellen.aparecidams@outlook.com>
2024-07-26 19:56:12 -0300

    typescript

--- src/app/shared/form-busca/form-busca.component.ts
+++ src/app/shared/form-busca/form-busca.component.ts
@@ -1,19 +1,20 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ModalComponent } from '../modal/modal.component';
-
+import { FormBuscaService } from 'src/app/core/services/form-busca.service';
 @Component({
   selector: 'app-form-busca',
   templateUrl: './form-busca.component.html',
   styleUrls: ['./form-busca.component.scss']
 })
 export class FormBuscaComponent {
-
-    constructor(public dialog: MatDialog) {}
-  
-    openDialog() {
-      this.dialog.open(ModalComponent);
-    }
+  constructor(public dialog: MatDialog, 
+    private formBuscaService : FormBuscaService) {}
+    
+  openDialog() {
+    this.dialog.open(ModalComponent, {
+      width: '50%'
+    })
   }
-
+}
 
