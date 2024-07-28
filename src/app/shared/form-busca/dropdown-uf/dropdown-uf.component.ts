a678a6627394baec6c93a8025f347a1e82dac7e2
Hellen Moura <hellen.aparecidams@outlook.com>
2024-07-28 18:35:30 -0300

    obter dados da api ao invés de exibir código estático

--- src/app/shared/form-busca/dropdown-uf/dropdown-uf.component.ts
+++ src/app/shared/form-busca/dropdown-uf/dropdown-uf.component.ts
@@ -1,13 +1,31 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { UnidadeFederativaService } from 'src/app/core/services/unidade-federativa.service';
+import { UnidadeFederativa } from 'src/app/core/types/type';
 
 @Component({
   selector: 'app-dropdown-uf',
   templateUrl: './dropdown-uf.component.html',
   styleUrls: ['./dropdown-uf.component.scss']
 })
-export class DropdownUfComponent {
-  @Input() label: string='';
-  @Input() iconePrefixo: string='';
 
-  filteredOptions= []
+export class DropdownUfComponent implements OnInit {
+  @Input() label: string = '';
+  @Input() iconePrefixo: string = '';
+
+  unidadesFederativas: UnidadeFederativa[] = [];
+
+  filteredOptions = []
+
+  constructor(
+    private unidadeFederativaService: UnidadeFederativaService) {
+
+  }
+
+  ngOnInit(): void {
+    this.unidadeFederativaService.listar()
+        .subscribe(dados => {
+            this.unidadesFederativas = dados
+            console.log(this.unidadesFederativas)
+        })
+}
 }
