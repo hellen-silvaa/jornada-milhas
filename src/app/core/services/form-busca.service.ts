caa4760e60865a93b61cbff5c07eeee52d5584fc
Hellen Moura <hellen.aparecidams@outlook.com>
2024-07-29 15:33:04 -0300

    gerenciando o controle da quantidade de passageiros

--- src/app/core/services/form-busca.service.ts
+++ src/app/core/services/form-busca.service.ts
@@ -17,7 +17,10 @@ export class FormBuscaService {
       somenteIda: new FormControl(false),
       origem: new FormControl(null),
       destino: new FormControl(null),
-      tipo: new FormControl("Executiva")
+      tipo: new FormControl("Executiva"),
+      adultos: new FormControl(1),
+      criancas: new FormControl(0),
+      bebes: new FormControl(0),
     })
   }
 
