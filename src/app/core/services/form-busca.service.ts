7bf79a99633de7e8d9dc46f85cc74941fb2d210d
Hellen Moura <hellen.aparecidams@outlook.com>
2024-07-29 15:24:24 -0300

    testando

--- src/app/core/services/form-busca.service.ts
+++ src/app/core/services/form-busca.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { MatChipSelectionChange } from '@angular/material/chips';
 import { MatDialog } from '@angular/material/dialog';
 import { ModalComponent } from 'src/app/shared/modal/modal.component';
 
@@ -10,25 +11,36 @@ export class FormBuscaService {
 
   formBusca: FormGroup;
 
-  constructor(private dialog: MatDialog) { 
+  constructor(private dialog: MatDialog) {
 
     this.formBusca = new FormGroup({
       somenteIda: new FormControl(false),
       origem: new FormControl(null),
       destino: new FormControl(null),
+      tipo: new FormControl("Executiva"),
     })
   }
 
-  obterControle(nome:string): FormControl {
+  obterControle(nome: string): FormControl {
     const control = this.formBusca.get(nome);
     if (!control) {
       throw new Error(`FormControl com nome "${nome}" não existe.`);
     }
     return control as FormControl;
   }
-  openDialog() {
-    this.dialog.open(ModalComponent, {
-      width: '50%'
-    })
-}
-}
+
+  alterarTipo(evento: MatChipSelectionChange, tipo: string) {
+    if (evento.selected) {
+      this.formBusca.patchValue({
+        tipo,
+      })
+      console.log('Tipo de passagem alterado para: ', tipo)
+    }
+  }
+
+    openDialog() {
+      this.dialog.open(ModalComponent, {
+        width: '50%'
+      })
+    }
+  }
