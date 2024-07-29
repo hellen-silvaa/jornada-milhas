b20139d5135232f98eed17f504fa122650bdadf1
Hellen Moura <hellen.aparecidams@outlook.com>
2024-07-29 15:41:46 -0300

    muito complicado, mas consegui

--- src/app/core/services/form-busca.service.ts
+++ src/app/core/services/form-busca.service.ts
@@ -27,9 +27,19 @@ export class FormBuscaService {
   getDescricaoPassageiros(): string {
     let descricao = ''
     
-    const adultos = this.formBusca.get('adultos')?.value
+    const adultos = this.formBusca.get('adultos')?.value;
     if (adultos && adultos > 0) {
-        descricao += ''
+        descricao += `${adultos} adulto${adultos > 1 ? 's' : ''}`;
+    }
+
+    const criancas = this.formBusca.get('criancas')?.value;
+    if (criancas && criancas > 0) {
+        descricao += `${descricao ? ', ' : ''}${criancas} criança${criancas > 1 ? 's' : ''}`;
+    }
+
+    const bebes = this.formBusca.get('bebes')?.value;
+    if (bebes && bebes > 0) {
+        descricao += `${descricao ? ', ' : ''}${bebes} bebê${bebes > 1 ? 's' : ''}`;
     }
     
     return descricao
